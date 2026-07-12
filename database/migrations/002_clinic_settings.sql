9b63f48581834df2924f12cdd358765d99236570
Raja Suriyaraja SR Siddha <rajasr211221@gmail.com>
2026-07-12 13:51:08 +0530

    Create 002_clinic_settings.sql

---
+++ database/migrations/002_clinic_settings.sql
@@ -0,0 +1,14 @@
+INSERT INTO settings.clinic_settings (
+    clinic_name,
+    registration_no,
+    doctor_name,
+    doctor_registration_no,
+    ...
+)
+VALUES (
+    'Suriyaraja Siddha Health Care',
+    'ERODSID20200015331',
+    'Dr. Raja Suriyaraja',
+    '8283',
+    ...
+);
